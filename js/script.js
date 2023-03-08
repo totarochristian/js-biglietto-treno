1c061ecefd98797bd31c613efa3897626c72fc9b
totarochristian <totaro.christian.18@gmail.com>
2023-03-08 14:36:14 +0100

    Add the main goal to the top of script.js file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+---------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------   Main Goal   ------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+The program will ask the user for the number of kilometers he wants to travel and the age of the passenger.
+Based on this information, he will have to calculate the total price of the trip, according to these rules:
+  - the ticket price is defined on the basis of km (€0.21 per km)
+  - a 20% discount must be applied for minors
+  - a 40% discount should be applied for over 65s.
+The final price output is put out in human form (with up to two decimals, to indicate cents on the price).
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+*/
